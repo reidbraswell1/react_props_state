66828706ab41dc592668c8548cde6d89a01f6439
reid <reidbras@gmail.com>
2022-08-31 20:38:39 -0500

    Updating table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 const Table = (props) => {
   return (
     <tr>
-        <td>{props.id}</td><td>{props.color}</td><td>{props.hexColor}</td><td id={props.tableId} style={{backgroundColor: props.hexColor}}></td>
+        <td>{props.id}</td><td>{props.color}</td><td>{props.hexColor}</td><td id={props.tableId} onClick={props.onClicked} style={{backgroundColor: props.hexColor}}></td>
     </tr>
   );
 };
